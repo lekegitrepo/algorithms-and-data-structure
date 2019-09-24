5379955e5908f5a148fecb41cfcf4846d36127df
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-09-24 09:07:51 -0600

    add model solution

--- Breadth-First-Search.rb
+++ Breadth-First-Search.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def bfs(graph)
   # write your code here
   queue = [0]
@@ -16,12 +18,41 @@ def bfs(graph)
   result
 end
 
-p bfs({
-  0 => [2], 
-  1 => [4], 
-  2 => [5, 0, 3], 
-  3 => [2], 
-  4 => [1, 5], 
+# model solution
+def bfs(graph)
+  # write your code here
+  queue = [0]
+  result = []
+  visited = []
+
+  until queue.empty?
+    head = queue.shift
+    result << head
+    visited << head
+
+    not_visited = graph[head].reject { |node| visited.include? node }
+
+    visited += not_visited
+    queue += not_visited
+  end
+
+  result
+end
+
+p bfs(
+  0 => [2],
+  1 => [4],
+  2 => [5, 0, 3],
+  3 => [2],
+  4 => [1, 5],
   5 => [4, 2]
-})
+)
 # => [0, 2, 5, 3, 4, 1]
+
+bfs(0 => [1, 2], 1 => [0, 2], 2 => [0, 1, 3, 4, 5], 3 => [2, 4], 4 => [3, 2], 5 => [2])
+
+bfs(0 => [1, 2], 1 => [0, 3, 4], 2 => [0, 5, 6], 3 => [1], 4 => [1], 5 => [2], 6 => [2])
+
+bfs(0 => [3], 1 => [2, 3], 2 => [4, 1], 3 => [1, 0], 4 => [2])
+
+bfs(0 => [1, 2], 1 => [0, 3, 4], 2 => [0, 5, 6], 3 => [1], 4 => [1, 5], 5 => [2, 4], 6 => [2])
