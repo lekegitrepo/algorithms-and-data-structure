aeec7057304a0ad03e9e0aace16fd121713a59d5
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-09-25 07:20:35 -0600

    rubocop fix error

---
+++ Trees_and_Graphs-Depth_First_Search.rb
@@ -0,0 +1,13 @@
+def depth_first_search(graph)
+  # write your code here
+end
+
+p depth_first_search({
+  0 => [2], 
+  1 => [4], 
+  2 => [5, 0, 3], 
+  3 => [2], 
+  4 => [1, 5], 
+  5 => [4, 2]
+})
+# => [0, 2, 5, 4, 1, 3]
