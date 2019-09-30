c6e904122e6b448ffdbaafd18ab5594736f92739
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-09-30 08:31:27 -0600

    rubocop fix indentations

--- Trees_and_Graphs-Stacks_or_Recursion.rb
+++ Trees_and_Graphs-Stacks_or_Recursion.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 def tree_height(tree_as_list)
   # your code here
   i = 0
   left = 0
   right = 0
   while i < tree_as_list.length
-    if [2 * i + 1] != nil
+    if ![2 * i + 1].nil?
       i = 2 * i + 1
       left += 1
-    elsif [2 * i + 2] != nil
+    elsif ![2 * i + 2].nil?
       i = 2 * i + 2
       right += 1
     end
