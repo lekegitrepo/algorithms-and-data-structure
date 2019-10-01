dbd5f604e5714686d447a035fab79513f7b2747b
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-01 07:52:49 -0600

    implement search method using the hint provided in the challenge

--- Trees_and_Graphs-Is_it_a_Search_Tree.rb
+++ Trees_and_Graphs-Is_it_a_Search_Tree.rb
@@ -11,16 +11,29 @@ end
 
 def array_to_tree(array, index = 0)
   # use your function from the previous challenge
-  return nil if i >= array.length || array[index] == 0
+  return nil if index >= array.length || (array[index]).zero?
 
   node = Node.new(array[index])
   node.left = array_to_tree(array, 2 * index + 1)
   node.right = array_to_tree(array, 2 * index + 2)
+
+  node
 end
 
 def search_tree?(array)
   root = array_to_tree(array)
   # write your code here
+  search(root)
+end
+
+def search(node, min = nil, max = nil)
+  return true if node.nil?
+
+  return false if min && node.data < min
+
+  return false if max && node.data > max
+
+  search(node.left, min, node.data) && search(node.right, node.data, max)
 end
 
 puts search_tree?([10, 4, 12])
