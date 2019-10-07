d1fee4ffc0197cc7b4752f5b4b964c602bdf1620
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-07 09:48:58 -0600

    use idea of mohamed from my standup team to solve the challenge

--- Trees_and_Graphs-Find_the_Cycles.rb
+++ Trees_and_Graphs-Find_the_Cycles.rb
@@ -1,23 +1,43 @@
+# frozen_string_literal: true
+
 def graph_cycle?(graph)
   # write your code here
-  
+  cycle(graph)
 end
 
-puts graph_cycle?({
-  0=>[2], 
-  1=>[4], 
-  2=>[0, 5, 3], 
-  3=>[5, 2], 
-  4=>[5, 1], 
-  5=>[4, 2, 3]
-})
+def cycle(graph, node = 0, visited = [])
+  # write your code here
+  counter = 0
+
+  return false if graph[node].nil?
+
+  graph[node].each do |cur_node|
+    if visited.include?(cur_node)
+      counter += 1
+      return true if counter == 2
+    else
+      visited << cur_node
+      return true if cycle(graph, cur_node, visited)
+    end
+  end
+  false
+end
+
+puts graph_cycle?(
+  0 => [2],
+  1 => [4],
+  2 => [0, 5, 3],
+  3 => [5, 2],
+  4 => [5, 1],
+  5 => [4, 2, 3]
+)
 # => true
 
-puts graph_cycle?({
-  0=>[2], 
-  1=>[2], 
-  2=>[0, 1, 3, 4, 5], 
-  3=>[2], 
-  4=>[2]
-})
+puts graph_cycle?(
+  0 => [2],
+  1 => [2],
+  2 => [0, 1, 3, 4, 5],
+  3 => [2],
+  4 => [2]
+)
 # => false
