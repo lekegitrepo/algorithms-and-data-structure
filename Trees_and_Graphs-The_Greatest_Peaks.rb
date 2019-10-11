d665c78bd9b2355d73871b0f13729e18f40fbd42
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-11 08:13:37 -0600

    add solution for greatest peak

--- Trees_and_Graphs-The_Greatest_Peaks.rb
+++ Trees_and_Graphs-The_Greatest_Peaks.rb
@@ -1,7 +1,77 @@
 # frozen_string_literal: true
 
+# mohamed's solution
+def get_next(x, y, map)
+  col = map[y].length
+  row = map.length
+  max = map[x][y]
+  next_cell = [x, y]
+
+  if (x > 0) && (map[x - 1][y] > max)
+    max = map[x - 1][y]
+    next_cell = [x - 1, y]
+  end
+
+  if (y > 0) && (map[x][y - 1] > max)
+    max = map[x][y - 1]
+    next_cell = [x, y - 1]
+  end
+
+  if (x < row - 1) && (map[x + 1][y] > max)
+    max = map[x + 1][y]
+    next_cell = [x + 1, y]
+  end
+
+  if (y < col - 1) && (map[x][y + 1] > max)
+    max = map[x][y + 1]
+    next_cell = [x, y + 1]
+  end
+  return nil if next_cell[0] == x && next_cell[1] == y
+
+  next_cell
+end
+
+def dfs(x, y, map)
+  local_peak = [x, y]
+
+  loop do
+    res = get_next(local_peak[0], local_peak[1], map)
+    local_peak = res unless res.nil?
+    return local_peak if res.nil?
+  end
+end
+
+# def dfs(x, y, map)
+#   local_peak = [x, y]
+#   puts "local_peak value #{local_peak}"
+#   # puts "y value #{local_peak[1]}"
+
+#   loop do
+#     res = get_next(local_peak[0], local_peak[1], map)
+#     local_peak = res unless res.nil?
+#     if res.nil?
+#       puts "local_peak inner value #{local_peak}"
+#       return local_peak
+#     end
+#   end
+# end
+
 def greatest_peaks(map)
   # write your code here
+  peaks = Hash.new 0
+  map.length.times do |i|
+    map[i].length.times do |j|
+      peaks[dfs(i, j, map)] += 1
+    end
+  end
+  puts peaks
+  min = nil
+  max = nil
+  peaks.each do |_key, value|
+    min = value if min.nil? || (value < min)
+    max = value if max.nil? || (value > max)
+  end
+  [min, max]
 end
 
 p greatest_peaks(
