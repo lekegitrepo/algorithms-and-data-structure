f61adf5ad32dcaf3f34dd8b562b07ccbdc7d1d6b
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-14 13:03:22 -0600

    add Maps solution

---
+++ Maps_Sets_and_Hashes-Maps.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+def appears_most_times(array)
+  # write your code here
+  hash_map = {}
+
+  array.each do |item|
+    hash_map.key?(item) ? hash_map[item] += 1 : hash_map[item] = 1
+  end
+  hash_map.key(hash_map.values.max)
+end
+
+puts appears_most_times([1, 2, 3, 1, 5])
+# => 1
+
+puts appears_most_times([23, 43, 67, 88, 42, 35, 77, 88, 99, 11])
+# => 88
+
+puts appears_most_times([4376, -345, -345, 4376, -345, 84_945, 4376, -345, -26_509, 4376, 84_945, 84_945, -26_509, 896_341, 4376])
+# => 4376
