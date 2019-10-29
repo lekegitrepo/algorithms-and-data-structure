573ff6457de1e2679b4fc4f25f21d3060b86865d
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-29 10:21:56 -0600

    add model solution

--- Sorting-Algorithms/Sorting_Algorithms-Simple_Couting_Sort.rb
+++ Sorting-Algorithms/Sorting_Algorithms-Simple_Couting_Sort.rb
@@ -1,3 +1,5 @@
+
+# this has a bug
 # def simple_counting_sort(array)
 #   # write your code here
 #   counts = counting_sort(array)
@@ -18,6 +20,29 @@ def simple_counting_sort(array)
   counts = Array.new(max+1, 0)
   array.each { |n| counts[n] += 1 }
   (0...counts.size).map { |i| [i] * counts[i] }.flatten
+end
+
+# model solution
+def simple_counting_sort(array)
+  # write your code here
+  count_array = get_count_array(array)
+  
+  ordered_array = []
+  count_array.each_with_index do |count, element|
+    ordered_array.concat([element] * count)
+  end
+  
+  ordered_array
+end
+
+def get_count_array(numbers)
+    counts = Array.new(100, 0)
+  
+  numbers.each do |element|
+    counts[element] += 1
+  end
+  
+  counts
 end
 
 p simple_counting_sort([63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33])
