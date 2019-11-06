8bcd499276f112d6061a8a5a1f559bb5a3d24827
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-11-06 14:35:04 -0600

    add hint solution

---
+++ Sorting-Algorithms/Sorting_Algorithms-QuickSort_Advanced.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+def partition(array, begin_index, end_index)
+  pivot = array[end_index]
+  j = begin_index
+  (begin_index...end_index).each do |i|
+    next unless array[i] < pivot
+    array[i], array[j] = array[j], array[i]
+    j += 1
+  end
+  array[end_index], array[j] = array[j], array[end_index]
+  j
+end
+
+def advanced_quicksort(array, begin_index = 0, end_index = array.length - 1)
+  if begin_index < end_index
+    pivot = partition(array, begin_index, end_index)
+    puts array.join(' ')
+    advanced_quicksort(array, begin_index, pivot - 1)
+    advanced_quicksort(array, pivot + 1, end_index)
+  end
+end
+
+advanced_quicksort([1, 3, 9, 8, 2, 7, 5])
+# => 1 3 2 5 9 7 8
+#    1 2 3 5 9 7 8
+#    1 2 3 5 7 8 9
