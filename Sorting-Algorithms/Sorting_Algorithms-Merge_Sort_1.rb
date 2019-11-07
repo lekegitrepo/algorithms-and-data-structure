41e3651c6e066da60d9e460fa54d00d26c643898
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-11-07 12:02:08 -0600

    add model solution

--- Sorting-Algorithms/Sorting_Algorithms-Merge_Sort_1.rb
+++ Sorting-Algorithms/Sorting_Algorithms-Merge_Sort_1.rb
@@ -23,6 +23,30 @@ end
 #   sorted_arr + array1 + array2
 # end
 
+# model solution
+def merge_sort(array1, array2)
+  # write your code here
+  sorted_array = []
+  i = 0
+  j = 0
+
+  while i < array1.size && j < array2.size
+    if array1[i] < array2[j]
+      sorted_array << array1[i]
+      i += 1
+    else
+      sorted_array << array2[j]
+      j += 1
+    end
+  end
+
+  sorted_array.concat(array1[i..-1]) if i < array1.size
+
+  sorted_array.concat(array2[j..-1]) if j < array2.size
+
+  sorted_array
+end
+
 p merge_sort([1, 3, 9, 11], [2, 4, 6, 8])
 # => [1, 2, 3, 4, 6, 8, 9, 11]
 
