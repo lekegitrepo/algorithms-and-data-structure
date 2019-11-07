cd3b9d4d8856b6935c1dc6e163283a1cdf7207a4
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-11-07 11:54:48 -0600

    adapt my mentor's solution

--- Sorting-Algorithms/Sorting_Algorithms-Merge_Sort_1.rb
+++ Sorting-Algorithms/Sorting_Algorithms-Merge_Sort_1.rb
@@ -1,16 +1,27 @@
+# frozen_string_literal: true
+
+# my mentor's solution
 def merge_sort(array1, array2)
   # write your code here
-  comb_arr = array1 + array2
-  p comb_arr.min..comb_arr.max
-  merg_arr = Array.new(comb_arr.length, 0)
-  i = 0
-  while i < comb_arr.length
-  	merg_arr[comb_arr[i]] = comb_arr[i]
-  	i += 1
+  sorted_arr = []
+  until array1.empty? || array2.empty?
+    sorted_arr << (array1[0] < array2[0] ? array1.shift : array2.shift)
   end
-  # merg_arr.compact
-  merg_arr -= [0, nil]
+  sorted_arr + array1 + array2
 end
+
+# def merge_sort(array1, array2)
+#   # write your code here
+#   sorted_arr = []
+#   until array1.empty? || array2.empty?
+#     sorted_arr << if array1[0] < array2[0]
+#                     array1.shift
+#                   else
+#                     array2.shift
+#                   end
+#   end
+#   sorted_arr + array1 + array2
+# end
 
 p merge_sort([1, 3, 9, 11], [2, 4, 6, 8])
 # => [1, 2, 3, 4, 6, 8, 9, 11]
