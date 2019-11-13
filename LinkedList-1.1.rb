1b60cf1b083f141f81ba64077ab257ec69d12fd1
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-11-13 05:28:57 -0600

    minor update

--- LinkedList-1.1.rb
+++ LinkedList-1.1.rb
@@ -1,35 +1,37 @@
+# frozen_string_literal: true
+
 class Node
   attr_accessor :value, :next_node
-  
+
   def initialize(value, next_node = nil)
-	  @value = value
+    @value = value
     @next_node = next_node
   end
 end
 
 class LinkedList
-  #setup head and tail
+  # setup head and tail
   def initialize
     @head = nil
     @tail = nil
   end
-  
+
   def add(number)
     # your code here
     node = Node.new(number)
-        @head ? (@tail.next_node = node) : (@head = node)
-        @tail = node
+    @head ? (@tail.next_node = node) : (@head = node)
+    @tail = node
   end
 
   def get(index)
     # your code here
     if index < 0
-            nil
-        else
-            node = @head
-            index.times { node = node.next_node }
-            node.value
-        end
+      nil
+    else
+      node = @head
+      index.times { node = node.next_node }
+      node.value
+    end
   end
 end
 
