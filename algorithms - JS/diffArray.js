893de289b483d8ec7eacc09fe488174f2b03a70c
lekegitrepo <lekedevrepo@gmail.com>
2020-04-23 07:52:52 -0500

    add another solution for diffArray problem

--- algorithms - JS/diffArray.js
+++ algorithms - JS/diffArray.js
@@ -6,3 +6,12 @@ function diffArray(arr1, arr2) {
   })
   return newArr;
 }
+
+// freecodecamp solution.
+function diffArray(arr1, arr2) {
+  return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+  function diff(a, b) {
+    return a.filter(item => b.indexOf(item) === -1);
+  }
+}
