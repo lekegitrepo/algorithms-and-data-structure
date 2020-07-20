50ebad77137f1248db7f3fbf638a663829fc5a1b
lekegitrepo <lekedevrepo@gmail.com>
2020-07-20 15:59:56 -0500

    add another solution for dnaPair problem

--- algorithms - JS/dnaPairing.js
+++ algorithms - JS/dnaPairing.js
@@ -22,5 +22,19 @@ function pairElement(str) {
   return newDna;
 }
 
+function pairElement(str) {
+  //create object for pair lookup
+  var pairs = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C"
+  };
+  //split string into array of characters
+  var arr = str.split("");
+  //map character to array of character and matching pair
+  return arr.map(x => [x, pairs[x]]);
+}
+
 const pair = pairElement("GCG");
 console.log(pair);
